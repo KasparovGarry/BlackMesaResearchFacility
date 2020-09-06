54905518fda6caecf64cbbf7ed8583e83b5dff0e
Garry Kasparov <cgompany519yt@gmail.com>
2020-09-06 23:29:03 +0400

    navbar ready

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,20 @@
 const navSlide = () => {
     const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".navlinks");
+    const bg = document.querySelector(".navbar-darkBG");
 
-    burger.addEventListener("click")
+    burger.addEventListener("click", () => {
+       nav.classList.toggle("navActive");
+       burger.classList.toggle("change");
+       bg.classList.toggle("show");
+
+    });
+
+    bg.addEventListener("click", () => {
+        nav.classList.toggle("navActive");
+        burger.classList.toggle("change");
+        bg.classList.remove("show");
+    });
 }
+
+navSlide();
