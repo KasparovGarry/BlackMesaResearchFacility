3c3b5db24d1f0c82e11e680df6b83dc3765cb9df
Garry Kasparov <cgompany519yt@gmail.com>
2020-09-15 17:43:02 +0400

    garri

--- scripts/main.js
+++ scripts/main.js
@@ -1,51 +1,42 @@
 const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".navLinks");
-    const navLink = document.querySelectorAll(".navLink a");
-    const bg = document.querySelector(".navbar-darkBG");
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".navLinks");
+  const navLink = document.querySelectorAll(".navLink a");
+  const bg = document.querySelector(".navbar-darkBG");
 
-    burger.addEventListener("click", () => {
-       nav.classList.toggle("navActive");
-       burger.classList.toggle("change");
-       bg.classList.toggle("show");
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("navActive");
+    burger.classList.toggle("change");
+    bg.classList.toggle("show");
+  });
 
+  bg.addEventListener("click", () => {
+    nav.classList.toggle("navActive");
+    burger.classList.toggle("change");
+    bg.classList.toggle("show");
+  });
+
+  navLink.forEach((navLink) => {
+    navLink.addEventListener("click", () => {
+      nav.classList.toggle("navActive");
+      burger.classList.toggle("change");
+      bg.classList.toggle("show");
     });
-
-    bg.addEventListener("click", () => {
-        nav.classList.toggle("navActive");
-        burger.classList.toggle("change");
-        bg.classList.toggle("show");
-    });
-
-    navLink.forEach((navLink) => {
-        navLink.addEventListener("click", () => {
-            nav.classList.toggle("navActive");
-            burger.classList.toggle("change");
-            bg.classList.toggle("show");
-        });
-    });
-}
+  });
+};
 
 navSlide();
 
 const scroll = () => {
-    const navbar = $("nav");
+  const navbar = $("nav");
 
-    if (navbar.scrollTop() > 0) {
-        navbar.classList.add("scrolled");
-    }
-}
-
-
+  if (navbar.scrollTop() > 0) {
+    navbar.classList.add("scrolled");
+  }
+};
 
 $(window).scroll(function () {
-    $("nav").toggleClass("scrolled", $(this).scrollTop() > 0);
+  $("nav").toggleClass("scrolled", $(this).scrollTop() > 0);
 });
 
 scroll();
-
-
-
-
-
-
